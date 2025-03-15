38951b9c8d016baae47ffefb0efac45c76e4eaf4
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-03-15 16:25:35 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public static void SapaUser<T>(T X)
+    {
+        Console.WriteLine("Halo user " + X);
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser("Abiyyu");
+
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("103022300121");
+        dataGeneric.printData();
+    }
+}
--- obj/Debug/net8.0/tpmodul5_103022300121.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul5_103022300121.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_103022300121")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d6d5f0a6ddab04c97fc30bb2bfb368688dffa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_103022300121")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_103022300121")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
